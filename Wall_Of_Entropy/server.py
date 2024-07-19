1f617eebd2086d35f7980489eff61287ba5bc326
voidcs <jamesrngsawang@gmail.com>
2024-07-19 00:05:55 -0700

    testing stream

--- Wall_Of_Entropy/server.py
+++ Wall_Of_Entropy/server.py
@@ -1,52 +1,56 @@
 import cv2
+import numpy as np
 import hashlib
-from flask import Flask, jsonify
+import time
+from argon2 import PasswordHasher
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 rtsp_url = os.getenv("rtsp_url")
-print(rtsp_url)
-app = Flask(__name__)
 
-@app.route('/random_number', methods=['GET'])
-def get_random_number():
-    print("request received")
-    frame_hash = capture_rtsp_stream(rtsp_url)
-    return jsonify({"random_number": frame_hash})
 
 def capture_rtsp_stream(rtsp_url):
-    print("in capture: ", rtsp_url)
     cap = cv2.VideoCapture(rtsp_url)
 
     if not cap.isOpened():
         print(f"Error: Could not open video stream with URL {rtsp_url}")
-        return None
+        return
 
-    ret, frame = cap.read()
-    if not ret:
-        print("Error: Failed to capture frame.")
-        cap.release()
-        return None
+    last_print_time = time.time()
 
-    frame_hash = process_frame(frame)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        current_time = time.time()
+        if current_time - last_print_time >= 5:
+            # Process the frame to generate entropy
+            process_frame(frame)
+            last_print_time = current_time
+
+        # Display the frame
+        cv2.imshow('RTSP Stream', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
-    return frame_hash
+    cv2.destroyAllWindows()
 
 def process_frame(frame):
     height, width, _ = frame.shape
 
-    overall_hash = hashlib.sha3_256() 
-    # hash the concatanetion of the pixels, then make a rolling hash basically
+    overall_hash = hashlib.sha3_256()
     for i in range(height):
         for j in range(width):
             r, g, b = frame[i, j]
             pixel = f"{r:03}{g:03}{b:03}".encode('utf-8')
             overall_hash.update(pixel)
-    return overall_hash.hexdigest()
+    print(overall_hash.hexdigest())
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
     capture_rtsp_stream(rtsp_url)
